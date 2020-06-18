ab8ef9eb0fcfa3f8dea4e72174f9b631fd0adb8c
Johan Arias <j@eldato.co.cr>
2020-06-17 21:48:26 -0600

    Update to generics in Inventory.java

--- src/Inventory.java
+++ src/Inventory.java
@@ -1,57 +1,57 @@
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
 public class Inventory {
-  private List guitars;
+    private List<Guitar> guitars;
 
-  public Inventory() {
-    guitars = new LinkedList();
-  }
+    public Inventory() {
+        guitars = new LinkedList<Guitar>();
+    }
 
-  public void addGuitar(String serialNumber, double price,
-                        String builder, String model,
-                        String type, String backWood, String topWood) {
-    Guitar guitar = new Guitar(serialNumber, price, builder,
-                               model, type, backWood, topWood);
-    guitars.add(guitar);
-  }
-  public Guitar getGuitar(String serialNumber) {
-    for (Iterator i = guitars.iterator(); i.hasNext(); ) {
-      Guitar guitar = (Guitar)i.next();
-      if (guitar.getSerialNumber().equals(serialNumber)) {
-        return guitar;
-      }
+    public void addGuitar(String serialNumber, double price,
+                          String builder, String model,
+                          String type, String backWood, String topWood) {
+        Guitar guitar = new Guitar(serialNumber, price, builder,
+                                   model, type, backWood, topWood);
+        guitars.add(guitar);
     }
-    return null;
-  }
-  public Guitar search(Guitar searchGuitar) {
-    for (Iterator i = guitars.iterator(); i.hasNext(); ) {
-      Guitar guitar = (Guitar)i.next();
-      // Ignore serial number since that's unique
-      // Ignore price since that's unique
-      String builder = searchGuitar.getBuilder();
-      if ((builder != null) && (!builder.equals("")) &&
-          (!builder.equals(guitar.getBuilder())))
-        continue;
-      String model = searchGuitar.getModel();
-      if ((model != null) && (!model.equals("")) &&
-          (!model.equals(guitar.getModel())))
-        continue;
-      String type = searchGuitar.getType();
-      if ((type != null) && (!searchGuitar.equals("")) &&
-          (!type.equals(guitar.getType())))
-        continue;
-      String backWood = searchGuitar.getBackWood();
-      if ((backWood != null) && (!backWood.equals("")) &&
-          (!backWood.equals(guitar.getBackWood())))
-        continue;
-      String topWood = searchGuitar.getTopWood();
-      if ((topWood != null) && (!topWood.equals("")) &&
-          (!topWood.equals(guitar.getTopWood())))
-        continue;
-      return guitar;
+    public Guitar getGuitar(String serialNumber) {
+        for (Guitar g : guitars) {
+            Guitar guitar = g;
+            if (guitar.getSerialNumber().equals(serialNumber)) {
+                return guitar;
+            }
+        }
+        return null;
     }
-    return null;
-  }
+
+    public Guitar search(Guitar searchGuitar) {
+        for (Guitar g : guitars) {
+            Guitar guitar = g;
+            // Ignore serial number since that's unique
+            // Ignore price since that's unique
+            String builder = searchGuitar.getBuilder();
+            if ((builder != null) && (!builder.equals("")) &&
+                (!builder.equals(guitar.getBuilder())))
+                continue;
+            String model = searchGuitar.getModel();
+            if ((model != null) && (!model.equals("")) &&
+                (!model.equals(guitar.getModel())))
+                continue;
+            String type = searchGuitar.getType();
+            if ((type != null) && (!type.equals("")) &&
+                (!type.equals(guitar.getType())))
+                continue;
+            String backWood = searchGuitar.getBackWood();
+            if ((backWood != null) && (!backWood.equals("")) &&
+                (!backWood.equals(guitar.getBackWood())))
+                continue;
+            String topWood = searchGuitar.getTopWood();
+            if ((topWood != null) && (!topWood.equals("")) &&
+                (!topWood.equals(guitar.getTopWood())))
+                continue;
+            return guitar;
+        }
+        return null;
+    }
 }
